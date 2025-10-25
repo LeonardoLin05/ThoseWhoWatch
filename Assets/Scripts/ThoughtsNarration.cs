aa0c5df19be83d966d57c39436dd05de8139f2dd
Leonardo Lin <leonardo.2812d05@gmail.com>
2025-10-25 12:54:59 +0200

    Pensamientos entre escenas
    - Se ha hecho el script para asignar y hacer los pensamientos del jugador en los cambios de escenas

---
+++ Assets/Scripts/ThoughtsNarration.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+
+public class ThoughtsNarration : MonoBehaviour
+{
+
+    [SerializeField] private string[] lineas;
+
+    private TextMeshProUGUI pensamientos;
+    private TextMeshProUGUI textoInteractuar2;
+
+    private Animator fade;
+
+    private int numeroLinea;
+    
+
+    void Awake()
+    {
+        pensamientos = gameObject.GetComponent<TextMeshProUGUI>();
+        numeroLinea = 1;
+    }
+
+    void Start()
+    {
+        CameraMovement.Instance.enabled = false;
+        PlayerMovement.Instance.enabled = false;
+        Interaction.Instance.enabled = false;
+
+        fade = GameObject.Find("Fade").GetComponent<Animator>();
+        textoInteractuar2 = GameObject.Find("texto_interactuar2").GetComponent<TextMeshProUGUI>();
+        textoInteractuar2.text = "[Espacio] para avanzar";
+        pensamientos.SetText(lineas[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AvanzarPensamiento();
+        }
+    }
+    
+    private void AvanzarPensamiento()
+    {
+        if (numeroLinea >= lineas.Length)
+        {
+            fade.SetTrigger("FadeOut");
+            textoInteractuar2.text = "";
+            gameObject.SetActive(false);
+
+            CameraMovement.Instance.enabled = true;
+            PlayerMovement.Instance.enabled = true;
+            Interaction.Instance.enabled = true;
+        }
+        else
+        {
+            // Usar la palabra saltar para empezar de nuevo con el siguiente texto borrando
+            // todo lo anterior
+            if (lineas[numeroLinea].CompareTo("saltar") == 0)
+            {
+                pensamientos.text = lineas[++numeroLinea];
+            }
+            else
+            {
+                pensamientos.text = pensamientos.text + "\n" + lineas[numeroLinea];
+            }
+            numeroLinea++; 
+        }
+    }
+}
