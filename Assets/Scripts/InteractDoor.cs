3e2a221674b4520d5a7a0fda8ad1e6fc78c3138c
Leonardo Lin <leonardo.2812d05@gmail.com>
2025-10-27 21:35:25 +0100

    Pensamientos escena

--- Assets/Scripts/InteractDoor.cs
+++ Assets/Scripts/InteractDoor.cs
@@ -1,27 +1,52 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class InteractDoor : MonoBehaviour, IInteractable
 {
+    public bool bloqueada;
+    [SerializeField] private string frasePuertaBloqueada;
+    private bool pensamientoTerminado = true;
+
     private Animator door;
     private bool open = false;
+    private TextMeshProUGUI pensamientos;
 
     void Start()
     {
         door = GetComponent<Animator>();
+        pensamientos = GameObject.Find("Pensamiento").GetComponent<TextMeshProUGUI>();
     }
 
     public IEnumerator interact()
     {
-        open = !open;
-        door.SetBool("open", open);
-        door.SetTrigger("interact");
+        if (!bloqueada)
+        {
+            open = !open;
+            door.SetBool("open", open);
+            door.SetTrigger("interact");
+            Debug.Log("El objecto ha hecho algo");
+            yield break;
+        }
+        else
+        {
+            if(!VariablesGlobales.EN_PENSAMIENTO)
+            StartCoroutine(puertaBloqueada());
+        }
         VariablesGlobales.INTERACTUAR = true;
-        Debug.Log("El objecto ha hecho algo");
-        yield break;
     }
 
-    public string MensajeInteraccion(){
+    private IEnumerator puertaBloqueada()
+    {
+        pensamientoTerminado = true;
+        pensamientos.text = frasePuertaBloqueada;
+        yield return new WaitForSeconds(2f);
+        pensamientos.text = "";
+        VariablesGlobales.EN_PENSAMIENTO = false;
+    }
+
+    public string MensajeInteraccion() {
         if (!open)
         {
             return "[E] para Abrir";
