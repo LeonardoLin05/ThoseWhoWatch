e33e1a2c483d4b9f581f41ced8727c808ba31863
VigilantePasa <danix05d@gmail.com>
2025-11-06 00:53:07 +0100

    Casa final Actualiza y Script Parpadeo

---
+++ Assets/Scripts/ParpadeoLuz.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParpadeoLuz : MonoBehaviour
+{
+    public Light luz;
+    public Renderer bombilla;           
+    public float minIntensidad = 0.2f;  
+    public float maxIntensidad = 1.5f;  
+    public float velocidad = 0.1f;  
+
+    void Start()
+    {
+        luz = GetComponent<Light>();
+        StartCoroutine(Parpadeo());
+    }
+
+    IEnumerator Parpadeo()
+    {
+        while (true)
+        {
+            luz.intensity = Random.Range(minIntensidad, maxIntensidad);
+
+            if (Random.value < 0.1f)
+            {
+                bombilla.material.SetColor("_Color" ,new Color(90f / 255f, 70f / 255f, 30f / 255f)); // No funciona
+                luz.enabled = false;
+                yield return new WaitForSeconds(1f); 
+            }
+            else
+            {
+                bombilla.material.SetColor("_Color" , new Color(205f / 255f, 170f / 255f, 69f / 255f)); // no funciona
+                luz.enabled = true;
+                yield return new WaitForSeconds(velocidad); 
+            }
+        }
+    }
+}
