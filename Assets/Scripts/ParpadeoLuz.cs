27fd4d0b082b51b4d4a27aa29f75759bcb5be8a8
VigilantePasa <danix05d@gmail.com>
2025-11-06 19:30:11 +0100

    Autobús

--- Assets/Scripts/ParpadeoLuz.cs
+++ Assets/Scripts/ParpadeoLuz.cs
@@ -19,8 +19,10 @@ public class ParpadeoLuz : MonoBehaviour
     {
         while (true)
         {
+            /*
             luz.intensity = Random.Range(minIntensidad, maxIntensidad);
 
+            
             if (Random.value < 0.1f)
             {
                 bombilla.material.SetColor("_Color" ,new Color(90f / 255f, 70f / 255f, 30f / 255f)); // No funciona
@@ -32,6 +34,26 @@ public class ParpadeoLuz : MonoBehaviour
                 bombilla.material.SetColor("_Color" , new Color(205f / 255f, 170f / 255f, 69f / 255f)); // no funciona
                 luz.enabled = true;
                 yield return new WaitForSeconds(velocidad); 
+            }
+            */
+            luz.enabled = true;
+            luz.intensity = maxIntensidad;
+
+            if (Random.value < 0.2f)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                     luz.intensity = Random.Range(minIntensidad, maxIntensidad);
+                }
+            }
+            if (Random.value < 0.1f)
+            {
+                luz.enabled = false;
+                yield return new WaitForSeconds(0.1f); 
+            }
+            else
+            {
+                yield return new WaitForSeconds(1f);
             }
         }
     }
