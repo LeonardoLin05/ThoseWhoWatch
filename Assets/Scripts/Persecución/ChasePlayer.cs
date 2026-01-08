3e45c8dcb1010224e6dfd966a8e20831ed6eb54a
Leonardo Lin <leonardo.2812d05@gmail.com>
2026-01-08 17:59:38 +0100

    Mini mejoras screamer persecución

--- Assets/Scripts/Persecución/ChasePlayer.cs
+++ Assets/Scripts/Persecución/ChasePlayer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,10 +6,13 @@ public class ChasePlayer : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private AudioSource screamer;
+    [SerializeField] private AudioSource musicaPersecucion;
     [SerializeField] private StressReceiver stressReceiverScript;
     [SerializeField] private TraumaInducer traumaInducerScritp;
 
     private NavMeshAgent npc;
+
+    private bool inCoroutine = false;
 
     void OnEnable()
     {
@@ -22,15 +26,27 @@ public class ChasePlayer : MonoBehaviour
 
     void Update()
     {
-        if(!screamer.isPlaying && Vector3.Distance(npc.transform.position, target.position) <= 7)
+        if(!inCoroutine && Vector3.Distance(npc.transform.position, target.position) <= 7)
         {
-            screamer.Play();
-            // Desactivamos el headbob y hacemos que la camara
-            // se agite dependiendo de la distancia con el NPC
-            HeadbobSystem.Instance.enabled = false;
-            stressReceiverScript.enabled = true;
-            traumaInducerScritp.enabled = true;
+            StartCoroutine(StartScream());
         }
         npc.destination = target.position;
+    }
+
+    private IEnumerator StartScream()
+    {
+        inCoroutine = true;
+
+        if(musicaPersecucion.isPlaying) {
+            musicaPersecucion.Stop();
+            yield return new WaitForSeconds(1f);
+        }
+        screamer.Play();
+
+        // Desactivamos el headbob y hacemos que la camara
+        // se agite dependiendo de la distancia con el NPC
+        HeadbobSystem.Instance.enabled = false;
+        stressReceiverScript.enabled = true;
+        traumaInducerScritp.enabled = true;
     }
 }
